95ac2e8061bb02ab291b9b91ea9567140e9c6474
Vita <vitalia@myrambler.ru>
2020-02-06 12:53:57 +0300

    - old template

--- flask-backend/main.py
+++
@@ -1,9 +0,0 @@
-import flask  
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return flask.render_template("index.html",token="hello")
-
-if __name__ == '__main__':
-    app.run()
