120483c1b92bb6bb54e3db4052e1bfca38b592ae
Your Name <33329377+imciflam@users.noreply.github.com>
2020-02-10 11:34:43 +0300

    + gzip

--- boilerplate/gulpfile.js
+++ boilerplate/gulpfile.js
@@ -6,21 +6,23 @@ var minify = require("gulp-csso")
 var buffer = require("vinyl-buffer")
 var source = require("vinyl-source-stream")
 var uglify = require("gulp-uglify")
+// var gzip = require("gulp-gzip")
 
-gulp.task("build", function() {
+gulp.task("react-prod", function() {
   process.env.NODE_ENV = "production"
-
-  return browserify({
-    entries: "./client/components/index.js",
-    extensions: [".jsx"],
-    debug: true
-  })
-    .transform("babelify", { presets: ["env", "react"] })
-    .bundle()
-    .pipe(source("bundle.js"))
-    .pipe(buffer())
-    .pipe(uglify())
-    .pipe(gulp.dest("static"))
+  return (
+    browserify({
+      entries: "./client/components/index.js",
+      debug: true
+    })
+      .transform("babelify", { presets: ["env", "react"] })
+      .bundle()
+      .pipe(source("bundle.js"))
+      .pipe(buffer())
+      .pipe(uglify())
+      //.pipe(gzip())
+      .pipe(gulp.dest("static"))
+  )
 })
 
 gulp.task("react", function(done) {
@@ -47,18 +49,7 @@ gulp.task("sass", function() {
     .pipe(gulp.dest("./static/css"))
 })
 
-gulp.task("apply-prod-environment", function(done) {
-  process.stdout.write("Setting NODE_ENV to 'production'" + "\n")
-  process.env.NODE_ENV = "production"
-  if (process.env.NODE_ENV != "production") {
-    throw new Error("Failed to set NODE_ENV to production!!!!")
-  } else {
-    process.stdout.write("Successfully set NODE_ENV to production" + "\n")
-  }
-  done()
-})
-
-gulp.task("serve", gulp.series("apply-prod-environment", "sass", "react"))
+gulp.task("serve", gulp.series("sass", "react-prod"))
 
 gulp.task("dev", function() {
   gulp.watch("client/**/*.scss", gulp.series("sass")),
