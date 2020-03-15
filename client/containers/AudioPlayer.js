ce8978dbbfc406dbee945b95f38ed780ed4a3a0a
Vita <vitalia@myrambler.ru>
2020-03-15 18:04:19 +0300

    + track switching and layout - scss

--- client/containers/AudioPlayer.js
+++ client/containers/AudioPlayer.js
@@ -1,32 +1,52 @@
 import React, { Component } from "react";
 
 export class AudioPlayer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentTrack: 0
+    };  
+  }
+
+  switchTrack(key){
+    this.setState({currentTrack: key})
+  }
+
   render() {
     console.log(this.props);
     return (
-      <div>
-        AudioPlayer
-        <ul>
+      <div className="audioplayer">
+        <h1 className="visually-hidden">AudioPlayer</h1>
+        <div className="audioplayer__title">
+          <p className="audioplayer__title--name">{this.props.tracks[this.state.currentTrack][1]} </p>
+          <p className="audioplayer__title--artist">{this.props.tracks[this.state.currentTrack][0]} </p>
+        </div>
+        <div className="audioplayer-container"> 
+        <div className="audioplayer__cover">
+            <img className="audioplayer__image" src={this.props.tracks[this.state.currentTrack][3]} 
+            alt="audioplayer image" />
+        </div>
+        <ul className="audioplayer__list"> 
           {this.props.tracks.map(element => {
             return (
-              <li key={this.props.tracks.indexOf(element)}>
-                <p>{element[0]}</p>
-                <p>{element[1]}</p>
-                <p>
-                  <audio controls>
-                    <source src={element[2]} />
-                    <source src="audio/music.mp3" type="audio/mpeg" />
-                    Тег audio не поддерживается вашим браузером.
-                    <a href="audio/music.mp3">Скачайте музыку</a>.
-                  </audio>
-                </p>
-                <p>
-                  <img src={element[3]} width="450" height="450" alt="" />
-                </p>
+              <li
+                key={this.props.tracks.indexOf(element)}
+                className="audioplayer__item"
+                onClick = {()=>{this.switchTrack(this.props.tracks.indexOf(element))}}
+              >
+                <div>
+                <p className="audioplayer__name">{element[0]}</p>
+                <p className="audioplayer__artist">{element[1]}</p></div> 
               </li>
-            );
+            ); 
           })}
         </ul>
+        </div>
+        <div className="audioplayer__audio-container">
+                  <audio className="audioplayer__audio-kit" controls>
+                    <source src={this.props.tracks[this.state.currentTrack][2]}  />
+                  </audio>
+                </div> 
       </div>
     );
   }
