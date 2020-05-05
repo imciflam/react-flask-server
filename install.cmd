2f6b037b572605b6b868d311f7550f437f4c6bec
Vita <vitalia@myrambler.ru>
2020-05-05 12:58:25 +0300

    + cmds for quick run&install

---
+++ install.cmd
@@ -0,0 +1,8 @@
+start /d spotify-flask-server pip install -r requirements.txt &
+start /d spotify-flask-server npm i &
+start /d music-collaborative-filtering pip install -r requirements.txt &
+start /d spotify-flask-server npm i &
+start /d music-genre-classifier pip install -r requirements.txt &
+start /d spotify-flask-server npm i &
+
+read x
---
+++ run.cmd
@@ -0,0 +1,5 @@
+start /d spotify-flask-server python index.py &
+start /d  music-collaborative-filtering python index.py &
+start /d music-genre-classifier python index.py &
+
+read x
